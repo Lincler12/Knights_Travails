2bf2ce881e25dfac701a3e58f49ebebbed42e8f4
Lincler12 <astrikort24@gmail.com>
2021-11-18 19:28:19 +0200

    Format file and remove unneeded method

--- KnightPathFinder.rb
+++ KnightPathFinder.rb
@@ -1,101 +1,93 @@
-require_relative "PolyTreeNode"
+require_relative 'PolyTreeNode'
 class KnightPathFinder
+  def initialize(position_array)
+    @considered_positions = [position]
+    @root_node = PolyTreeNode.new(position_array)
+    build_move_tree
+  end
 
-    def initialize(position_array)
-        @considered_positions = [position]
-        @root_node = PolyTreeNode.new(position_array)
-        build_move_tree
+  def build_move_tree
+    queue = [@root_node]
+    until queue.empty?
+      node = queue.shift
+      process(node)
+      positions = new_move_positions(node.value)
+      node.children.each { |child_node| queue << child_node }
     end
+  end
 
-    def build_move_tree
+  def process(node)
+    positions = new_move_positions(node.value)
+    positions.each { |pos| PolyTreeNode.new(pos).parent = node }
+  end
 
-        queue = [@root_node]
-        until queue.empty?
-            node = queue.shift
-            process(node)
-            positions = new_move_positions(node.value)
-            node.children.each {|child_node| queue << child_node}
-        end
+  def self.valid_moves(pos)
+    row, col = pos
+    moves_array = []
+    if row == 0 and col == 0
+      moves_array << [row, col + 1]
+      moves_array << [row + 1, col]
+      moves_array << [row + 1, col + 1]
+    elsif row == 7 and col == 0
+      moves_array << [row - 1, col]
+      moves_array << [row - 1, col + 1]
+      moves_array << [row, col + 1]
+    elsif row == 0 and col == 7
+      moves_array << [row, col - 1]
+      moves_array << [row + 1, col - 1]
+      moves_array << [row + 1, col]
+    elsif row == 7 and col == 7
+      moves_array << [row - 1, col - 1]
+      moves_array << [row - 1, col]
+      moves_array << [row, col - 1]
+    elsif row == 0
+      moves_array << [row, col - 1]
+      moves_array << [row, col + 1]
+      moves_array << [row + 1, col - 1]
+      moves_array << [row + 1, col]
+      moves_array << [row + 1, col + 1]
+    elsif row == 7
+      moves_array << [row - 1, col - 1]
+      moves_array << [row - 1, col]
+      moves_array << [row - 1, col + 1]
+      moves_array << [row, col - 1]
+      moves_array << [row, col + 1]
+    elsif col == 0
+      moves_array << [row - 1, col]
+      moves_array << [row - 1, col + 1]
+      moves_array << [row, col + 1]
+      moves_array << [row + 1, col]
+      moves_array << [row + 1, col + 1]
+    elsif col == 7
+      moves_array << [row - 1, col - 1]
+      moves_array << [row - 1, col]
+      moves_array << [row, col - 1]
+      moves_array << [row + 1, col - 1]
+      moves_array << [row + 1, col]
+    else
+      moves_array << [row - 1, col - 1]
+      moves_array << [row - 1, col]
+      moves_array << [row - 1, col + 1]
+      moves_array << [row, col - 1]
+      moves_array << [row, col + 1]
+      moves_array << [row + 1, col - 1]
+      moves_array << [row + 1, col]
+      moves_array << [row + 1, col + 1]
     end
+    moves_array
+  end
 
-    def process(node)
-        positions = new_move_positions(node.value)
-        positions.each { |pos| PolyTreeNode.new(pos).parent = node }
-            
-    end
+  def new_move_positions(pos)
+    return nil if pos.nil?
 
-    def self.valid_moves(pos)
-        row,col = pos
-        moves_array = []
-        if row == 0 and col == 0
-            moves_array << [row, col + 1]
-            moves_array << [row + 1, col]
-            moves_array << [row + 1, col + 1]
-        elsif row == 7 and col == 0
-            moves_array << [row - 1, col]
-            moves_array << [row - 1, col + 1]
-            moves_array << [row, col + 1]
-        elsif row == 0 and col == 7
-            moves_array << [row, col - 1]
-            moves_array << [row + 1, col - 1]
-            moves_array << [row + 1, col]
-        elsif row == 7 and col == 7
-            moves_array << [row - 1, col - 1]
-            moves_array << [row - 1, col]
-            moves_array << [row, col - 1]
-        elsif row == 0
-            moves_array << [row, col - 1]
-            moves_array << [row, col + 1]
-            moves_array << [row + 1, col - 1]
-            moves_array << [row + 1, col]
-            moves_array << [row + 1, col + 1]
-        elsif row == 7 
-            moves_array << [row - 1, col - 1]
-            moves_array << [row - 1, col]
-            moves_array << [row - 1, col + 1]
-            moves_array << [row, col - 1]
-            moves_array << [row, col + 1]
-        elsif col == 0
-            moves_array << [row - 1, col]
-            moves_array << [row - 1, col + 1]
-            moves_array << [row, col + 1]
-            moves_array << [row + 1, col]
-            moves_array << [row + 1, col + 1]
-        elsif col == 7
-            moves_array << [row - 1, col - 1]
-            moves_array << [row - 1, col]
-            moves_array << [row, col - 1]
-            moves_array << [row + 1, col - 1]
-            moves_array << [row + 1, col]
-        else
-            moves_array << [row - 1, col - 1]
-            moves_array << [row - 1, col]
-            moves_array << [row - 1, col + 1]
-            moves_array << [row, col - 1]
-            moves_array << [row, col + 1]
-            moves_array << [row + 1, col - 1]
-            moves_array << [row + 1, col]
-            moves_array << [row + 1, col + 1]
-        end
-        moves_array
+    correct_moves_array = []
+    moves_array = valid_moves(pos)
+    moves_array.each do |position|
+      unless @considered_positions.include?(position)
+        correct_moves_array << position
+        @considered_positions << position
+      end
     end
-
-    check_positions(pos)
-        row,col = pos
-        if row == 0 and col == 0
-        end
-
-    end
-    def new_move_positions(pos)
-            return nil if pos.nil?
-            correct_moves_array = []
-            moves_array = self.valid_moves(pos)
-            moves_array.each do |position|
-                unless @considered_positions.include?(position)
-                    correct_moves_array << position 
-                    @considered_positions << position
-                end
-            end
-            correct_moves_array
-    end
+    correct_moves_array
+  end
 end
